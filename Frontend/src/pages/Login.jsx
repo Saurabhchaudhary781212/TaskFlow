2b8506265ad58ab79235c2d961419e15f37550d7
Saurabhchaudhary781212 <saurabhchaudhary4021@gmail.com>
2026-07-21 22:47:44 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import "./Login.css";
+import "./login.css";
 
 const Login = () => {
   const { login } = useAuth();
